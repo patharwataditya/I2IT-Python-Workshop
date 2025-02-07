62be9f5c17368bc81aa35b8fa40b58b3650978cc
Aditya6032 <adityaup6032@gmail.com>
2025-02-07 10:30:39 +0530

    Initial Commit

---
+++ main.py
@@ -0,0 +1,16 @@
+import numpy as np
+import pandas as pd
+import streamlit as st
+
+st.title("Welcome Everyone we are learning Python")
+st.write("Python Requires Practice!!!")
+
+data = pd.DataFrame({'c1':[10, 20, 30, 40], 'c2':['A', 'B', 'C', 'D']})
+
+st.write("Below is Table for Data")
+st.write(data)
+
+chart_data = pd.DataFrame(np.random.randn(20,3),
+                          columns=['A','B','C'])
+
+st.bar_chart(chart_data)
