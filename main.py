3edb6db41e77f0ea089cbd7dc0c84e5113345f3f
Aditya6032 <adityaup6032@gmail.com>
2025-02-07 10:43:08 +0530

    Uhh

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ data = pd.DataFrame({'c1':[10, 20, 30, 40], 'c2':['A', 'B', 'C', 'D']})
 st.write("Below is Table for Data")
 st.write(data)
 
+st.write("Nilya BSDK")
 chart_data = pd.DataFrame(np.random.randn(20,3),
                           columns=['A','B','C'])
 
